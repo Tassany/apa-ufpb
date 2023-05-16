7d0e915743fc4a3e5a519c2a3b8bc4ccda81c5a0
Joao Victor <dev.02@pbhub.com.br>
2023-05-16 11:50:32 -0300

    lendo array e matriz, e ordenando o array

--- main.cpp
+++ main.cpp
@@ -2,6 +2,39 @@
 #include <fstream>
 #include <string>
 
+void quicksort(int *array, int esq, int dir)
+{
+    int i = esq, j = dir;
+    int pivo = array[(esq + dir) / 2];
+
+    while (i <= j)
+    {
+        // encontrar o primeiro elemento maior que o pivo
+        while (array[i] < pivo)
+            i++;
+
+        // encontrar o primeiro elemento menor que o pivo
+        while (array[j] > pivo)
+            j--;
+
+        // swap dos elementos encontrados acima
+        if (i <= j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    };
+
+    // recursao para as duas metades do array
+    if (esq < j)
+        quicksort(array, esq, j);
+    if (i < dir)
+        quicksort(array, i, dir);
+}
+
 int main()
 {
     std::string fileName = "arquivo.txt"; // Nome do arquivo a ser lido
@@ -26,11 +59,35 @@ int main()
 
     inputFile >> numLinhasProducao >> numProdutos;
 
-    std::cout << "Numero de linhas de producao: " << numLinhasProducao << std::endl;
-    std::cout << "Numero de produtos: " << numProdutos << std::endl;
+    // ler um array do tamanho do numero de produtos
+    int *produtos = new int[numProdutos];
+
+    for (int i = 0; i < numProdutos; i++)
+    {
+        inputFile >> produtos[i];
+    }
+
+    // ler uma matriz de tamanho numProdutos x numProdutos
+    int **matriz = new int *[numProdutos];
+    for (int i = 0; i < numProdutos; i++)
+    {
+        matriz[i] = new int[numProdutos];
+    }
 
     // Fecha o arquivo
     inputFile.close();
+
+    std::cout << "Numero de linhas de producao: " << numLinhasProducao << std::endl;
+    std::cout << "Numero de produtos: " << numProdutos << std::endl;
+
+    // ordenar o array de produtos
+    quicksort(produtos, 0, numProdutos - 1);
+
+    std::cout << "Array ordenado: ";
+    for (int i = 0; i < numProdutos; i++)
+    {
+        std::cout << produtos[i] << " ";
+    }
 
     return 0;
 }
